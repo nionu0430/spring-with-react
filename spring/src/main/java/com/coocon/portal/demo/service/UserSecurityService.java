7535bfed916ba57988527ca5d508071d6d85d57e
LAPTOP-SL0JNDB7\NAMJAEMIN <nionu0430@gmail.com>
2022-12-09 13:41:20 +0900

    feat:spring security setting
    add spring security config file
    add springSecurityService - implements User
    
    SPRING-3

---
+++ spring/src/main/java/com/coocon/portal/demo/service/UserSecurityService.java
@@ -0,0 +1,12 @@
+package com.coocon.portal.demo.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserSecurityService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
