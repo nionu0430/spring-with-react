de9ec6bd1f6ac4c1fbc4f4faf0b75d113d6d6193
jaem <nionu0430@gmail.com>
2022-12-20 23:27:51 +0900

    feat:add spring rest docs
    add spring rest docs settings to build.gradle
    and sample controller method , test codes
    
    SPRING-4

--- spring/src/test/java/com/coocon/portal/demo/user/UserControllerTest.java
+++ spring/src/test/java/com/coocon/portal/demo/user/UserControllerTest.java
@@ -4,13 +4,15 @@ import com.coocon.portal.demo.Util.JsonUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
